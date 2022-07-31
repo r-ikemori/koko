4aec64e96e3bfc7ccb7e52b716c1df45ed54ccab
r-ikemori <dragon06051996@gmail.com>
2022-07-31 11:56:44 +0000

    [add]mypage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,26 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "homes#top"
+
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  # 顧客用
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+ scope module: :public do
+   root to: 'homes#top'
+   get 'customers/mypage', :to => "customers#show", as: "customers"
+   get 'customers/mypage/edit', :to => "customers#edit", as: "edit_customers"
+   patch 'customers/mypage', :to => "customers#update"
+   get 'customers/mypage/confirm', :to => "customers#confirm"
+   patch 'customers/mypage/unsubscribe', :to => "customers#unsubscribe"
+ end
+
+
+# 管理者用
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
 end
